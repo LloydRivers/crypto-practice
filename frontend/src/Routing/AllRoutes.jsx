605dc16e67a0f8f24fcc9ae6de4c24d6fa34263d
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-22 22:35:04 +0100

    Update Allroutes

--- frontend/src/Routing/AllRoutes.jsx
+++ frontend/src/Routing/AllRoutes.jsx
@@ -5,14 +5,7 @@ const AllRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route>
-        </Route>
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
   );

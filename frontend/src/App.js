ca1c0aa22af413b51431793aefff1674e92e82c9
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-22 22:36:24 +0100

    test endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import { NFTCard } from "./Components";
+import { useEffect } from "react";
+
+import { getData } from "utils/API/FetchNFTS/getData.js";
 
 function App() {
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
       <NFTCard />
